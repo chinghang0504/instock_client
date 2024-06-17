2853aa2509602a0771a8803476029a93f77ba6e9
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-06-17 15:46:01 -0700

    update old css imports to scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 import { BrowserRouter, Routes, Route,} from "react-router-dom";
 import Warehouse from './pages/Warehouse/Warehouse';
 import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
