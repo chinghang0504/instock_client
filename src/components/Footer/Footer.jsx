94004d18f2836d84f728333ba36290da2d570ac4
Harpreet Singh <singhprogram17@gmail.comit remote add origin https://github.com/singh1300/Harpreet-Jagra-travel-site.git>
2024-06-18 07:07:15 -0700

    header and footer created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <section className="footer">
+        <p className="footer__heading">
+            InStock Inc. All Rights Reserved.
+        </p>
+    </section>
+  )
+}
+
+export default Footer;
