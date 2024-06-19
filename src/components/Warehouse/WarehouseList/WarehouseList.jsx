da96408d89368f1518eecf88f1821b29982961ef
Ching Hang Lam <chinghang0504@gmail.com>
2024-06-19 14:38:24 -0700

    Update the style of the Warehouse List Header

--- src/components/Warehouse/WarehouseList/WarehouseList.jsx
+++ src/components/Warehouse/WarehouseList/WarehouseList.jsx
@@ -3,10 +3,12 @@ import { getWarehouseList } from '../../../services/api.js';
 import deleteIcon from '../../../assets/icons/delete_outline-24px.svg';
 import editIcon from '../../../assets/icons/edit-24px.svg';
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import searchIcon from '../../../assets/icons/search-24px.svg';
 
 function WarehouseList() {
   const [warehouseList, setWarehouseList] = useState([]);
+  const searchInputRef = useRef();
 
   useEffect(() => {
     // Load the data
@@ -17,35 +19,46 @@ function WarehouseList() {
     loadData();
   }, []);
 
+  function clickSearchIcon() {
+    searchInputRef.current.focus();
+  }
+
   return (
     <div className='warehouse-list'>
-      <Link to="/warehouse/details">Details</Link>
-      <div className='warehouse-list__header'>
-        <h1 className='warehouse-list__title'>Warehouse</h1>
-        <input className='warehouse-list__search-input' type="text" placeholder='Search...' />
-        <button className='warehouse-list__add-button'>Add New Warehouse</button>
-      </div>
-      <div className='warehouse-list__list'>
-        {warehouseList.map(warehouse => {
-          return (
-            <div className='warehouse-list__item' key={warehouse.id}>
-              <p>WAREHOUSE</p>
-              <p>{warehouse.warehouse_name}</p>
-              <p>ADDRESS</p>
-              <p>{warehouse.address}</p>
-              <p>CONTACE NAME</p>
-              <p>{warehouse.contact_name}</p>
-              <p>contact information</p>
-              <p>{warehouse.contact_phone}</p>
-              <p>{warehouse.contact_email}</p>
-              <div>
-                <img src={deleteIcon} alt="delete icon" />
-                <img src={editIcon} alt="edit icon" />
-              </div>
+      <div className='warehouse-list__container'>
+        <div className='warehouse-list__header'>
+          <h1 className='warehouse-list__title'>Warehouses</h1>
+          <div className='warehouse-list__action-container'>
+            <div className='warehouse-list__search-container'>
+              <input className='warehouse-list__search-input' ref={searchInputRef} type="text" placeholder='Search...' />
+              <img className='warehouse-list__search-icon' src={searchIcon} alt="search icon" onClick={clickSearchIcon}/>
             </div>
-          );
-        })}
+            <Link className='warehouse-list__add-link' to='/warehouse/add'><button className='warehouse-list__add-button'>+ Add New Warehouse</button></Link>
+          </div>
+        </div>
+        <div className='warehouse-list__list'>
+          {warehouseList.map(warehouse => {
+            return (
+              <div className='warehouse-list__item' key={warehouse.id}>
+                <p>WAREHOUSE</p>
+                <p>{warehouse.warehouse_name}</p>
+                <p>ADDRESS</p>
+                <p>{warehouse.address}</p>
+                <p>CONTACE NAME</p>
+                <p>{warehouse.contact_name}</p>
+                <p>contact information</p>
+                <p>{warehouse.contact_phone}</p>
+                <p>{warehouse.contact_email}</p>
+                <div>
+                  <img src={deleteIcon} alt="delete icon" />
+                  <img src={editIcon} alt="edit icon" />
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
+
     </div>
   )
 }
