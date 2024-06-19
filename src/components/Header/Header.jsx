82a793329e8cedc117fdad33b5404927036575d5
Harpreet Singh <singhprogram17@gmail.comit remote add origin https://github.com/singh1300/Harpreet-Jagra-travel-site.git>
2024-06-19 13:59:12 -0700

    header fixed and navlink updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,56 @@
-import React, { useState } from 'react'
-import './Header.scss'
-import Logo from '../../assets/logo/InStock-Logo_2x.png';
+import React, { useState } from "react";
+import "./Header.scss";
+import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/logo/InStock-Logo_2x.png";
+import { NavLink } from "react-router-dom";
 
 function Header() {
-    const [currentPage, setCurrentPage] = useState("warehouse");
+//   const [currentPage, setCurrentPage] = useState("warehouse");
+//   const navigate = useNavigate();
+
+//   const handleNavigation = (page) => {
+//     setCurrentPage(page);
+//     navigate(`/${page}`);
+//   };
+
   return (
     <section className="header">
-        <section className="header__nav">
-            <div className="header__logo-section">
-                <img src={Logo} alt="" className="header__logo" />
-            </div>
+      <section className="header__nav">
+        <div className="header__logo-section">
+            <NavLink to="/">
+            <img src={Logo} alt="InStock Logo" className="header__logo" />
+            </NavLink>
+        </div>
 
-            <div className="header__list">
-                <div className="header__warehouse-section">
-                    <button onClick={() => setCurrentPage("warehouse")} className={`header__warehouse header__modifier ${currentPage === "warehouse" ? 'header--active': ""}`}>Warehouses</button>
-                </div>
-                <div className="header__inventory-section">
-                    <button onClick={() => setCurrentPage("inventory")} className={`header__inventory header__modifier ${currentPage === "inventory" ? 'header--active': ""}`} >Inventory</button>
-                </div>
-            </div>
-        </section>
+        <div className="header__list">
+          <div className="header__warehouse-section">
+            <NavLink 
+              to="/warehouse"
+              className={({isActive}) =>
+              `   header__warehouse header__modifier ${
+                  isActive ? "header--active" : ""
+                }`                
+            }
+            >
+                Warehouses
+            </NavLink>
+          </div>
+          <div className="header__inventory-section">
+          <NavLink 
+              to="/inventory"
+              className={({isActive}) =>
+              `   header__inventory header__modifier ${
+                  isActive ? "header--active" : ""
+                }`                
+            }
+            >
+                Inventory
+            </NavLink>
+          </div>
+        </div>
+      </section>
     </section>
-  )
+  );
 }
 
 export default Header;
