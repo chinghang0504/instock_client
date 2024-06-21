3a84e59547327b2149c3e159f332dc3976750c3d
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-06-21 13:46:35 -0700

    add api call to post inventory in service folder

--- src/services/api.js
+++ src/services/api.js
@@ -3,22 +3,15 @@ import axios from "axios";
 // Global constants
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
+
+// ------------WAREHOUSE
+
 // Get the warehouse list
 export async function getWarehouseList() {
     const res = await axios.get(`${BASE_URL}/warehouse`);
     return res.data;
-}
-// Get the inventory list
-export async function getInventoryList() {
-    const res = await axios.get(`${BASE_URL}/inventory`);
-    return res.data;
-}
+};
 
-// Get the inventory
-export async function getInventoryEdit(id) {
-    const res = await axios.get(`${BASE_URL}/inventory/${id}`);
-    return res.data;
-}
 // Delete warehouse 
 export async function deleteWarehouse (currentItemId) {
     try {
@@ -28,6 +21,28 @@ export async function deleteWarehouse (currentItemId) {
         console.error('Error deleting warehouse:', error);
         throw error;
     }
+};
+
+// Get warehouse search
+export async function getWarehouseSearch(searchTerm = '') {
+    const res = await axios.get(`${BASE_URL}/warehouse/search`, {
+        params: { s: searchTerm }
+    });
+    return res.data;
+};
+
+//--------------INVENTORY
+
+ // Get the inventory list
+export async function getInventoryList() {
+    const res = await axios.get(`${BASE_URL}/inventory`);
+    return res.data;
+};
+
+// Get the inventory
+export async function getInventoryEdit(id) {
+    const res = await axios.get(`${BASE_URL}/inventory/${id}`);
+    return res.data;
 };
 
 // Delete inventory
@@ -41,12 +56,14 @@ export async function deleteInventory (currentItemId) {
     }
 };
 
-// Get warehouse search
-export async function getWarehouseSearch(searchTerm = '') {
-    const res = await axios.get(`${BASE_URL}/warehouse/search`, {
-        params: { s: searchTerm }
-    });
-    return res.data;
+//Create inventory 
+export async function createInventory () {
+    try{
+        const response = await axios.post (`${BASE_URL}/inventory`)
+    } catch (error){
+        console.error('Error creating inventory:', error);
+        throw error;
+    }
 };
 
 // Get inventory search 
