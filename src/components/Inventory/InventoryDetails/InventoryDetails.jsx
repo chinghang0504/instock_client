53e9c2f96825c0a2421a9e7683d18577cdd1412b
Ching Hang Lam <chinghang0504@gmail.com>
2024-06-21 15:07:22 -0700

    Update the style of Inventory Details Header

--- src/components/Inventory/InventoryDetails/InventoryDetails.jsx
+++ src/components/Inventory/InventoryDetails/InventoryDetails.jsx
@@ -1,8 +1,38 @@
+import { Link, useParams } from 'react-router-dom';
 import './InventoryDetails.scss'
+import { useEffect, useState } from 'react';
+import { getInventoryEdit } from '../../../services/api';
+import arrowBackIcon from '../../../assets/icons/arrow_back-24px.svg';
+import editIcon from '../../../assets/icons/edit-24px.svg';
 
 function InventoryDetails() {
+  const params = useParams();
+  const [inventoryData, setInventoryData] = useState({});
+
+  // Execute once
+  useEffect(() => {
+    async function loadData() {
+      const data = await getInventoryEdit(params.id);
+      setInventoryData(data);
+    }
+    loadData();
+  }, []);
+
   return (
-    <div>InventoryDetails</div>
+    <div className='inventory-details'>
+      <div className='inventory-details__container'>
+        <div className='inventory-details-header'>
+        <div className='inventory-details-header__container'>
+            <Link className='inventory-details-header__arrow-back-link' to='/inventory'><img className='inventory-details-header__arrow-back-icon' src={arrowBackIcon} alt="Back to inventory list" /></Link>
+            <h1 className='inventory-details-header__title'>{inventoryData.item_name}</h1>
+          </div>
+          <Link className='inventory-details-header__edit-link' to={`/inventory/edit/${inventoryData.id}`}><img className='inventory-details-header__edit-icon' src={editIcon} alt="Edit inventory details" /><span className='inventory-details-header__edit-text'>Edit</span></Link>
+        </div>
+        <div className='inventory-details-content'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
